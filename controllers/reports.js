ec5256784e9125b56e2f0f4fbc846e8a88be46fe
Andrew Jordan <humandrift@gmail.com>
2019-01-26 17:17:55 -0500

    Listing reports that are missing alt tags

--- controllers/reports.js
+++ controllers/reports.js
@@ -1,6 +1,28 @@
 const express = require('express')
 const router = express.Router()
+const fs = require('fs')
 
-router.get('/results', (req, res) => res.send("Report results here."))
+// the path of the folder containing the reports
+const path = "./reports/"
 
+router.get('/results', (req, res) => {
+  let viewModel = {
+        reports: [],
+  }
+
+  fs.readdir(path, (err, files) => {
+    if (err) return console.log(err)
+    files.forEach((file, index) => {
+      let report = require("." + path + file)
+      if(report.audits["image-alt"].score == 0) {
+        viewModel.reports[index] = {
+          report: file,
+          message: report.audits["image-alt"].title
+        }
+        console.log(viewModel.reports)
+      }
+    })
+  })
+  res.render("results", viewModel)
+})
 module.exports = router
--- server.js
+++ server.js
@@ -1,29 +1,29 @@
 const express = require('express')
 const app = express()
-const fs = require('fs')
+
 const hbs  = require('express-handlebars');
 const url = require('url')
 const dateFormat = require('dateformat')
 const port = 3000
 
 // include json report
-const path = "./reports/"
+//const path = "./reports/"
 //const report = require("./reports/20190114www_marist_edu_about_presidents_office_speeches_writings_inaugural_address.json")
 
-var reader = function (req, res, next) {
-  fs.readdir("./reports", (err, files) => {
-  files.forEach(file => {
-    let report = require(path + file)
-    if(report.audits["image-alt"].score == 0) {
-      console.log(file)
-      console.log(report.audits["image-alt"].title)
-    }
-  });
-})
-  next()
-}
+// var reader = function (req, res, next) {
+//   fs.readdir("./reports", (err, files) => {
+//   files.forEach(file => {
+//     let report = require(path + file)
+//     if(report.audits["image-alt"].score == 0) {
+//       console.log(file)
+//       console.log(report.audits["image-alt"].title)
+//     }
+//   });
+// })
+//   next()
+// }
 
-app.use(reader)
+//app.use(reader)
 
 // include routes
 app.use(require('./controllers'))
@@ -31,4 +31,4 @@ app.use(require('./controllers'))
 app.engine('handlebars', hbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Light Reader is listening on port ${port}!`))
